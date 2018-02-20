b147fddf843afe9e6ef6b61970c8445859ddbee2
troels <kamp991@gmail.com>
2018-02-19 17:00:16 -0800

    removed quickcheck cuz overload of <<

--- random_correct_test.cpp
+++ random_correct_test.cpp
@@ -5,9 +5,7 @@
 #include <boost/random/uniform_int_distribution.hpp>
 
 #include "bfpstatic2.cpp"
-#include "quickcheck/quickcheck/quickcheck.hh"
 
-using namespace quickcheck;
 using namespace std;
 
 
@@ -20,9 +18,9 @@ int main(){
 
     BFPStatic<int8_t,4> A{{3, 18, 119, 27}, 0};
     BFPStatic<int8_t,4> B{{2, -79, 98, -104}, 0};
-    BFPStatic<int8_t, 100> ABC = gen_bfp<int8_t, 100>();
+    BFPStatic<int64_t, 100> ABC = gen_bfp<int64_t, 100>();
 
-    cout << ABC << endl;
+    cout << ABC.to_float() << endl;
     // check_add(A,A);
     // check_add(Afp,Afp);
     // check_add(A,B);
