f527d4f75f2e9078dcb0938a477e3fee08c821a1
troels <kamp991@gmail.com>
2018-02-27 15:31:38 -0800

    gen_bfp_no_0 added

--- random_correct_test.cpp
+++ random_correct_test.cpp
@@ -15,20 +15,35 @@ BFPStatic<T,N> gen_bfp(boost::random::mt19937 &rng) {
     for(size_t i = 0; i < N; i++){
         elems[i] = rand_elem(rng);
     }
-    boost::random::uniform_int_distribution<T> rand_exp(0, 10); //rand_exp(numeric_limits<T>::min(), numeric_limits<T>::max());
+    boost::random::uniform_int_distribution<T> rand_exp(-10, 10); //rand_exp(numeric_limits<T>::min(), numeric_limits<T>::max());
+    BFPStatic<T,N> A(elems, rand_exp(rng)); //rand_exp(rng)
+    // BFPStatic<T,N> A(elems, 0);
+    return A; //BFPStatic<T,N>(A.to_float());
+}
+
+template <typename T, size_t N>
+BFPStatic<T,N> gen_bfp_no_0(boost::random::mt19937 &rng) {
+    array<T,N> elems;
+    boost::random::uniform_int_distribution<T> rand_elem(numeric_limits<T>::min(), numeric_limits<T>::max());
+    for(size_t i = 0; i < N; i++){
+        auto re = rand_elem(rng);
+        while(re == 0)
+            re = rand_elem(rng);
+        elems[i] = re;
+    }
+    boost::random::uniform_int_distribution<T> rand_exp(-10, 10); //rand_exp(numeric_limits<T>::min(), numeric_limits<T>::max());
     BFPStatic<T,N> A(elems, rand_exp(rng)); //rand_exp(rng)
     // BFPStatic<T,N> A(elems, 0);
     return A; //BFPStatic<T,N>(A.to_float());
 }
 
 
-
 int main(){
     boost::random::mt19937 rng;
     rng.seed(time(NULL));
 
-    auto A = gen_bfp<int16_t, 100>(rng);
-    auto B = gen_bfp<int16_t, 100>(rng);
+    auto A = gen_bfp_no_0<int8_t, 5>(rng);
+    auto B = gen_bfp_no_0<int8_t, 5>(rng);
 
     // BFPStatic<int8_t, 5> C{{-61, -71, 80, 1, 1}, 5};
     // BFPStatic<int8_t, 5> D{{120, -2, -121, 1, 1}, 10};
@@ -43,7 +58,7 @@ int main(){
     // auto Q = BFPStatic<int8_t, 3>(C.to_float());
     // cout << Q << endl;
     // check_add(A,A);
-    check_mul(A,B);
+    check_div(A,B);
     
 
     // + 0 5 5 6 6 6 7 7
