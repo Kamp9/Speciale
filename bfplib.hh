1348942087e08503c0c23e03ec5fb75312e08042
troels <kamp991@gmail.com>
2018-04-23 21:26:19 +0200

    dont forget bfplib

--- bfplib.hh
+++ bfplib.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <numeric>
 #include <inttypes.h>
 #include <stdio.h>
 
@@ -33,6 +34,43 @@ template <typename T> int floor_log2(T value)
 
   return i - 1;
 }
+
+// Should be unit tested since negative should get one off
+template <typename T> int calc_shifts(T num){
+	int r = 0;
+	num = abs(num);
+	while (num >>= 1) // unroll for more speed...
+	{
+	  r++;
+	}
+	return numeric_limits<T>::digits - r - 1;
+}
+
+
+// uint32_t v; // find the log base 2 of 32-bit v
+// int r;      // result goes here
+
+// static const int MultiplyDeBruijnBitPosition[32] = 
+// {
+//   0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+//   8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
+// };
+
+// v |= v >> 1; // first round down to one less than a power of 2 
+// v |= v >> 2;
+// v |= v >> 4;
+// v |= v >> 8;
+// v |= v >> 16;
+
+// r = MultiplyDeBruijnBitPosition[(uint32_t)(v * 0x07C4ACDDU) >> 27];
+
+// static const int MultiplyDeBruijnBitPosition2[32] = 
+// {
+//   0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+//   31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+// };
+// r = MultiplyDeBruijnBitPosition2[(uint32_t)(v * 0x077CB531U) >> 27];
+
 
 // template <typename T> msb(const &T x);
 
