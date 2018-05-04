b6ca924c98a3296508bc9b5f0c13d8446511a4c4
James Emil Avery <avery@diku.dk>
2018-05-04 12:44:13 +0200

    Quick benchmark of floor_log2

---
+++ log2_benchmark.cpp
@@ -0,0 +1,72 @@
+#include "bfplib.hh"
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <typeinfo>
+#include <time.h>
+#include <sys/time.h>
+#include <iostream>
+#include <chrono>
+
+template <typename T> void log2_benchmark(boost::random::mt19937 rng, int N)
+{
+  boost::random::uniform_int_distribution<T> rand_elem(numeric_limits<T>::min()+1, numeric_limits<T>::max());
+
+  int Nbits = sizeof(T)*8;
+  
+
+  //#define clock chrono::high_resolution_clock
+#define clock chrono::steady_clock
+
+  // We don't want to measure the random number generation together with the benchmark:  
+  auto start0 = clock::now();
+  cerr << "Generating random "<<Nbits<<"-bit numbers... ";
+  vector<T> random_T(N);
+  for(int i=0;i<N;i++)
+    random_T[i] = rand_elem(rng);
+  auto  end0  = clock::now();
+  cerr << chrono::duration<float,milli>(end0-start0).count() << "ms.\n";
+  
+  // Three methods
+  // 1) O(n) integer floor_log2_slow(x), 2) FPU (int)floor(log2(x)), and 3) New, fast integer floor_log2(x)
+  int msb;
+  cerr << "Calculating "<<N<<" calls to "<<Nbits<<"-bit floor_log2_slow(x)... ";
+  auto start1 = clock::now();
+  for(int i=0;i<N;i++)
+    msb = floor_log2_slow(random_T[i]);
+  auto  end1  = clock::now();
+  cerr << chrono::duration<float,milli>(end1-start1).count() << "ms.\n";
+
+  cerr << "Calculating "<<N<<" calls to "<<Nbits<<"-bit FPU floor(log2(x))... ";
+  auto start2 = clock::now();
+  for(int i=0;i<N;i++)
+    msb = floor(log2(random_T[i]));
+  auto  end2  = clock::now();
+  cerr << chrono::duration<float,milli>(end2-start2).count() << "ms.\n";
+
+  cerr << "Calculating "<<N<<" calls to "<<Nbits<<"-bit new floor_log2(x)... ";
+  auto start3 = clock::now();
+  for(int i=0;i<N;i++)
+    msb = floor_log2(random_T[i]);
+  auto  end3  = clock::now();
+  cerr << chrono::duration<float,milli>(end3-start3).count() << "ms.\n";    
+  
+  
+
+}
+
+int main()
+{
+  boost::random::mt19937 rng;
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  rng.seed(tv.tv_usec);
+  
+  const int N = 1000000000;
+
+  log2_benchmark<uint8_t> (rng,N);
+  log2_benchmark<uint16_t>(rng,N);
+  log2_benchmark<uint32_t>(rng,N);
+  log2_benchmark<uint64_t>(rng,N);    
+    
+  return 0;
+}
