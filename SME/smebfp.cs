898ff28339ea85a92f815518fcd1d60e0c4888f7
troels <kamp991@gmail.com>
2018-07-13 22:23:07 +0200

    adding with output bus

--- SME/smebfp.cs
+++ SME/smebfp.cs
@@ -33,120 +33,166 @@ public class BFPStatic {
     public int Len() { return elems.Count; }
 
 
+    public static void plus(int i, int A, int B, BFP.OutputLine C, int size) {
 
-    public static BFPStatic plus(BFPStatic A, BFPStatic B) {
-        if(A.exp < B.exp) return plus(B, A);
-        int size = 32;
-        BFPStatic AB = new BFPStatic(A.Len());
+        C.Elem[i] = (byte) (A + B);
 
-        int exp_diff = A.exp - B.exp;
-        bool carry = false;
+        // if(A[0] < B[0]) return plus(B, A, size);
+        // C.Elems[i.count] = A.Elems[i.count] + B.Elems[i.count];
 
+        // C.Elems[0] = (byte) ((byte) A.Elems[0] + (byte) B.Elems[0]);
+ 
         // int exp_diff2 = min(exp_diff, numeric_limits<T>::digits + 1);
         // int exp_diff2 =  Math.Min(exp_diff, size);
-        int exp_diff2 = size ^ ((exp_diff ^ size) & -Convert.ToInt32(exp_diff < size)); // min(x, y)
+        // int exp_diff2 = size ^ ((exp_diff ^ size) & -Convert.ToInt32(exp_diff < size)); // min(x, y)
 
-        // A.size()??
-        for(int i=0;i<A.Len();i++){
+        // // A.size()??
+        // for(int i=0;i<A.Len();i++){
 
-            long ABi = (((long) A[i]) << exp_diff2) + (long) B[i];
+        //     long ABi = (((long) A[i]) << exp_diff2) + (long) B[i];
 
-            ABi = (ABi >> (exp_diff2)) + ((ABi >> (exp_diff2 - 1)) & 1);
-            int abi = Convert.ToInt32(ABi);
-            carry |= (Convert.ToBoolean(A[i] >> (size - 1)) ^ Convert.ToBoolean(abi >> (size - 1))) & (Convert.ToBoolean(B[i] >> (size - 1)) ^ Convert.ToBoolean(abi >> (size - 1)));
-        }
+        //     ABi = (ABi >> (exp_diff2)) + ((ABi >> (exp_diff2 - 1)) & 1);
+        //     int abi = Convert.ToInt32(ABi);
+        //     carry |= (Convert.ToBoolean(A[i] >> (size - 1)) ^ Convert.ToBoolean(abi >> (size - 1))) & (Convert.ToBoolean(B[i] >> (size - 1)) ^ Convert.ToBoolean(abi >> (size - 1)));
+        // }
 
-        if(carry){
-            for(int i=0;i<A.Len();i++){
+        // if(carry){
+        //     for(int i=0;i<A.Len();i++){
 
-                bool sign = Convert.ToBoolean(((Convert.ToInt64(A[i]) << exp_diff2) + Convert.ToInt64(B[i])) >> (size - 1));
+        //         bool sign = Convert.ToBoolean(((Convert.ToInt64(A[i]) << exp_diff2) + Convert.ToInt64(B[i])) >> (size - 1));
 
-                long v = Convert.ToInt64(A[i]) << exp_diff2;
-                long abs_mask = v >> size - 1;
-                long ABi = ((v + abs_mask) ^ abs_mask) + Convert.ToInt64(B[i]);
+        //         long v = Convert.ToInt64(A[i]) << exp_diff2;
+        //         long abs_mask = v >> size - 1;
+        //         long ABi = ((v + abs_mask) ^ abs_mask) + Convert.ToInt64(B[i]);
 
-                // ABi = Math.Abs(ABi >> exp_diff2) - Convert.ToInt64(sign);
-                v = ABi >> exp_diff2;
-                abs_mask = v >> size - 1;
-                ABi = ((v + abs_mask) ^ abs_mask) + Convert.ToInt64(sign);
+        //         // ABi = Math.Abs(ABi >> exp_diff2) - Convert.ToInt64(sign);
+        //         v = ABi >> exp_diff2;
+        //         abs_mask = v >> size - 1;
+        //         ABi = ((v + abs_mask) ^ abs_mask) + Convert.ToInt64(sign);
 
-                bool rounding = Convert.ToBoolean(ABi & 1);
-                AB[i] = Convert.ToInt32((ABi >> 1) + Convert.ToInt64(rounding));
+        //         bool rounding = Convert.ToBoolean(ABi & 1);
+        //         AB[i] = Convert.ToInt32((ABi >> 1) + Convert.ToInt64(rounding));
 
-            }
-        }else{
-            for(int i=0;i<A.Len();i++){
+        //     }
+        // }else{
+        //     for(int i=0;i<A.Len();i++){
 
-                long ABi = (((long) A[i]) << exp_diff2) + (long) B[i];
-                bool rounding = Convert.ToBoolean((ABi >> (exp_diff2 - 1)) & 1) && (exp_diff2 > 0);
-                bool rounding2 = Convert.ToBoolean((ABi & ((1 << exp_diff2) -1)) == (1 << (exp_diff2 - 1))) && Convert.ToBoolean(ABi >> (size - 1));
+        //         long ABi = (((long) A[i]) << exp_diff2) + (long) B[i];
+        //         bool rounding = Convert.ToBoolean((ABi >> (exp_diff2 - 1)) & 1) && (exp_diff2 > 0);
+        //         bool rounding2 = Convert.ToBoolean((ABi & ((1 << exp_diff2) -1)) == (1 << (exp_diff2 - 1))) && Convert.ToBoolean(ABi >> (size - 1));
 
-                AB[i] = Convert.ToInt32((ABi >> exp_diff2) + Convert.ToInt32(rounding) - Convert.ToInt32(rounding2));
+        //         AB[i] = Convert.ToInt32((ABi >> exp_diff2) + Convert.ToInt32(rounding) - Convert.ToInt32(rounding2));
 
-            }
-        }
+        //     }
+        // }
 
-        AB.exp = A.exp + Convert.ToInt32(carry);
-        return AB;
+        // AB.exp = A.exp + Convert.ToInt32(carry);
+        // return AB;
     }
+
+
+
 
 [ClockedProcess]
 public class BFP : SimpleProcess {
-    // public interface IOperand : IBus {
-    //     [InitialValue(0)]
-    //     int OP { get; set; }
+
+    [TopLevelInputBus]
+    public interface InputCounter : IBus
+    {
+        [InitialValue(0)]
+        int count { get; set; }
+
+        [InitialValue(true)]
+        bool IsValid { get; set; }
+    }
+
+
+    // [TopLevelInputBus]
+    // public interface InputLineA : IBus
+    // {
+    //     [FixedArrayLength(2)]
+    //     IFixedArray<byte> Elems { get; set; }
     // }
 
-    public interface IInput : IBus {
-        [InitialValue(0)]
-        BFPStatic IN { get; set; }
-    }
 
-    public interface IResult : IBus {
-        BFPStatic OUT { get; set; }
-    }
+    // [TopLevelInputBus]
+    // public interface InputLineB : IBus
+    // {
+    //     [FixedArrayLength(2)]
+    //     IFixedArray<byte> Elems { get; set; }
+    // }
 
-    [InputBus]
-    IInput Input1 = Scope.CreateBus<IInput>();
-    // ISignal Input2 = Scope.CreateBus<ISignal>();
 
     [OutputBus]
-    IResult Output = Scope.CreateBus<IResult>();
+    // public interface OutputLine : IBus
+    // {
+    //     int Elem { get; set; }
+    //     // [FixedArrayLength(2)]
+    //     // IFixedArray<byte> Elems { get; set; }
+    // }
+
+
+    [TopLevelInputBus]
+    public interface OutputLine : IBus
+    {
+        [InitialValue]
+        bool IsValid { get; set; }
+
+        [FixedArrayLength(4)]
+        IFixedArray<byte> Elem { get; set; }
+    }
+
+    
+    private readonly byte[] InputA = { 100, 2, 3, 0};
+    private readonly byte[] InputB = { 4, 5, 6, 0};
+
+    [InputBus, OutputBus]
+    InputCounter Counter = Scope.CreateBus<InputCounter>();
+
+    // [InputBus]
+    // InputLineA InputA = Scope.CreateBus<InputLineA>();
+
+    // [InputBus]
+    // InputLineB InputB = Scope.CreateBus<InputLineB>();
+
+    [OutputBus]
+    OutputLine Output = Scope.CreateBus<OutputLine>();
 
     protected override void OnTick() {
-        Output.OUT = plus(Input1.IN, Input1.IN);
+
+        if (Counter.IsValid){
+            Counter.IsValid = false;
+
+            plus(Counter.count, InputA[Counter.count], InputB[Counter.count], Output, 2);
+            Counter.count++;
+
+            Counter.IsValid = true;
+        }
     }
 }
-
 
 [ClockedProcess]
 public class BFPrunner : Process {
 
-    [InputBus, OutputBus]
-    private readonly BFP.IInput Input = Scope.CreateOrLoadBus<BFP.IInput>();
-
-    // private readonly Q1.IOperand Operand = Scope.CreateOrLoadBus<Q1.IOperand>();
-    // private readonly Q1.ISignal Signal = Scope.CreateOrLoadBus<Q1.ISignal>();
-    // Operand.A = rand;
-
     [OutputBus]
-    private readonly BFP.IResult Result = Scope.CreateOrLoadBus<BFP.IResult>();
+    private readonly BFP.OutputLine Result = Scope.CreateOrLoadBus<BFP.OutputLine>();
+
+    // [InputBus, OutputBus]
+    // private readonly BFP.InputLineA InputA = Scope.CreateOrLoadBus<BFP.InputLineA>();
+    // private readonly BFP.InputLineB InputB = Scope.CreateOrLoadBus<BFP.InputLineB>();
+
 
     public async override Task Run() {
         await ClockAsync();
-        // List<int> a = new List<int>{1,2,3};
-        // List<int> b = new List<int>{4,5,6};
-        // BFPStatic A = new BFPStatic(a, 2);
-        // BFPStatic B = new BFPStatic(b, 2);
-        // Input1.IN = 4; //A;
-        Result.OUT = plus(Input.IN, Input.IN);
-
+        for(int i = 0; i < 3; i++){
+            await ClockAsync();
+            Console.WriteLine(Result.Elem[i]);
+        }
     }
 }
 
 public class SMEBFP {
     static void Main(string[] args) {
-        Console.WriteLine("ofjgeagodoh");
         new Simulation()
             // .BuildCSVFile()
             // .BuildGraph()
